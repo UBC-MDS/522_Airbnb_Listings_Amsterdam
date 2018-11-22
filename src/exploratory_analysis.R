4650567cb81ff1318542dffdd58fe0d12998101e
Gabriel Bogo <gabrielmedeirosbogo@gmail.com>
2018-11-22 14:43:07 -0800

    checkpoint before merging PR #17

--- src/exploratory_analysis.R
+++ src/exploratory_analysis.R
@@ -15,9 +15,10 @@ clean_listings <- clean_listings %>%
 clean_listings %>% ggplot(aes(x=number_of_reviews, y=price)) +
     geom_point()
 
-#Scatter plot between price and reviews
-clean_listings %>% ggplot(aes(x=number_of_reviews, y=price)) +
-  geom_point()
+#Scatter plot between price and minimum of nights
+clean_listings %>% ggplot(aes(x=minimum_nights, y=price)) +
+  geom_point() +
+  scale_x_log10()
 
 #Distribution of price per type of listing
 clean_listings %>% ggplot(aes(x=room_type, y=price)) +
